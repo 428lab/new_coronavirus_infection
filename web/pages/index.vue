25756e2617e536b4687c13a86056e4ad246651b1
Takuma Shinohara <takuma1201@gmail.com>
2020-03-17 11:44:39 +0900

    表示とレイアウト、文言を修正

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -4,96 +4,70 @@
       <h2 class="h3">免責事項</h2>
       <hr />
       <p class="mt-3">
-        この予測は、現時点での各国の感染者数・回復者数・死者数をもとにSEIRモデルを用いて今後の広がり方を予測したものとなります。<br />
-        SEIRモデルでは気温や湿度、また人の動きなどのパラメータはないため、環境側面等は一切考慮されません。<br />
-        あくまで現段階での実績数値をもとに、プログラムによる予測ということをご理解いただいた上でのグラフ閲覧をお願いしております。<br />
+        この予測は、現時点での各国の発症者数・回復者数・死者数をもとにSEIRモデルを用いて今後の広がり方を予測したものとなります。
+        <br />SEIRモデルでは気温や湿度、また人の動きなどのパラメータはないため、環境側面等は一切考慮されません。
+        <br />あくまで現段階での実績数値をもとに、プログラムによる予測ということをご理解いただいた上でのグラフ閲覧をお願いしております。
+        <br />
       </p>
       <p>
-        このグラフによる将来予測については一切の保証はありません。予測が外れたとしても、我々は一切それについて責務を追うことはありません。<br />
+        このグラフによる将来予測については一切の保証はありません。予測が外れたとしても、我々は一切それについて責務を追うことはありません。
+        <br />
       </p>
       <div class="text-center">
-        <button class="btn btn-lg btn-danger my-3" @click="modalShow = false">
-          私は上記の内容に了承します
-        </button>
+        <button class="btn btn-lg btn-danger my-3" @click="modalShow = false">私は上記の内容に了承します</button>
       </div>
     </b-modal>
     <div class="container">
       <div class="d-flex align-items-center justify-content-between">
-        <h1 class="h3">COVID-19 感染者数予測</h1>
-        <!-- <div>
-          <span class="p-2">
-            SNSでシェア
-          </span>
+        <h1 class="h3">COVID-19 発症者数予測</h1>
+        <div>
+          <span class="p-2">SNSでシェア</span>
           <a
             href="https://www.facebook.com/share.php?u=https://covid19.428lab.net"
-            class="text-center"
+            class="btn bg-facebook"
           >
-            <span class=" p-2 bg-facebook">
-              <img src="/blandlogo/facebook.svg" height="28px" />
-            </span>
+            <img src="/blandlogo/facebook.svg" height="28px" />
           </a>
           <a
-            href="https://twitter.com/share?text=新型コロナウイルスの感染者数を数理モデルで予測する - 四谷ラボ&url=https://covid19.428lab.net&hashtags=新型コロナウイルス,COVID-19"
-            class="text-center"
+            href="https://twitter.com/share?text=新型コロナウイルスの発症者数を数理モデルで予測する - 四谷ラボ&url=https://covid19.428lab.net&hashtags=新型コロナウイルス,COVID-19"
+            class="btn bg-twitter"
           >
-            <span class=" p-2 bg-twitter">
-              <img src="/blandlogo/twitter.svg" height="28px" />
-            </span>
+            <img src="/blandlogo/twitter.svg" height="28px" />
           </a>
-        </div> -->
+        </div>
       </div>
-      <div class="form-row align-items-center">
+      <!-- <div class="form-row align-items-center">
         <div class="col-auto">
-          <label for="staticEmail" class="col-form-label">Country : </label>
+          <label for="staticEmail" class="col-form-label">Country :</label>
         </div>
         <div class="col-auto">
-          <select
-            class="form-control"
-            v-model="selectCountry"
-            @change="setData(selectCountry)"
-          >
+          <select class="form-control" v-model="selectCountry" @change="setData(selectCountry)">
             <option v-for="item in countries" :key="item.id">{{ item }}</option>
           </select>
         </div>
-      </div>
+      </div>-->
+      <p>前回データ取得日時：{{ lastUpdate }}</p>
+      <p>発症者数のピーク(予測)：{{ peak.date }} 時点で {{ peak.max.toFixed() }}人</p>
+
       <!-- <button @click="setData()"></button> -->
-      <line-chart
-        v-if="estimation"
-        :chartdata="chartData"
-        :options="chartOptions"
-        class="mt-3"
-      />
-      <div class="mt-3">
-        <h3 class="h4">予測結果</h3>
-        <p>
-          感染のピーク：{{ peak.date }}<br />
-          感染者数：{{ peak.max.toFixed() }} 人
-        </p>
-        <p>前回データ取得日時：{{ lastUpdate }}</p>
-      </div>
+      <line-chart v-if="estimation" :chartdata="chartData" :options="chartOptions" class="mt-3" />
     </div>
     <footer class="mt-5 mb-3">
       <div class="container">
         <div>
           制作・運営:
-          <a href="https://twitter.com/SHINOHARATTT" target="_blank"
-            >T.Shinohara</a
-          >,
-          <a href="https://twitter.com/uesitananame55" target="_blank"
-            >Zinntikumugai</a
-          ><br />
-          開発リポジトリ:
+          <a href="https://twitter.com/SHINOHARATTT" target="_blank">T.Shinohara</a>,
+          <a href="https://twitter.com/uesitananame55" target="_blank">Zinntikumugai</a>
+          <br />開発リポジトリ:
           <a
             href="https://github.com/428lab/new_coronavirus_infection"
             target="_blank"
-            >GitHub</a
-          ><br />
-          もとのブログ記事:
+          >GitHub</a>
+          <br />もとのブログ記事:
           <a
             href="https://blog.428lab.net/entry/2020/03/02/080000"
             target="_blank"
-            >四谷ラボ公式ブログ</a
-          >
+          >四谷ラボ公式ブログ</a>
         </div>
         <div class="text-center mt-2">
           &copy; 2020 -
@@ -167,9 +141,11 @@ export default {
         .toFormat("YYYY/MM/DD");
       if (peak.max < e) {
         peak.max = e;
-        peak.date = date;
+        peak.date = new Date(firstDateString)
+          .add({ days: i })
+          .toFormat("MM月DD日");
       }
-      return date;
+      return new Date(firstDateString).add({ days: i }).toFormat("YYYY/MM/DD");
     });
 
     let fact = {};
@@ -216,12 +192,11 @@ export default {
           return this.est_data[country].fact[key][date];
         });
       });
-      console.log(fact);
       this.labels = labels;
       this.estimation = this.est_data[country].estimation;
       this.fact = fact;
       this.peak = peak;
-      this.lastUpdate = est_data[country]["last_update"];
+      this.lastUpdate = this.est_data[country]["last_update"];
     }
   },
   computed: {
@@ -230,7 +205,7 @@ export default {
         labels: this.labels,
         datasets: [
           {
-            label: "感染者数（推定）",
+            label: "発症者数（推定）",
             borderColor: "#F44",
             fill: false,
             pointRadius: 0,
@@ -261,7 +236,7 @@ export default {
           },
           {
             type: "bar",
-            label: "感染者数",
+            label: "発症者数",
             borderColor: "#E88",
             backgroundColor: "#E88",
             data: this.fact.infected,
