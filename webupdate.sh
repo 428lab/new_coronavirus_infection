93a9ffd906c9adab8feb0d55c4026735c390f470
zinntikumugai <uesitananame55@gmail.com>
2020-03-17 23:46:50 +0900

    fix

--- webupdate.sh
+++ webupdate.sh
@@ -3,6 +3,7 @@
 docker-compose build --no-cache
 docker-compose up -d
 docker-compose exec covid_19_seir_model bash update.sh
+docker-compose exec covid_19_seir_model bash -c "echo \"{}\" > output.json"
 docker-compose exec covid_19_seir_model bash generate.sh
 
 docker-compose run web_build ash -c "cd /var/www && ash update.sh"
