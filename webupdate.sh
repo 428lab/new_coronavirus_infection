4ab0c1a543306e6fd4d994ab5ffbd6feeaa3a1a0
zinntikumugai <uesitananame55@gmail.com>
2020-03-18 14:31:23 +0900

    fix webupdate.sh

--- webupdate.sh
+++ webupdate.sh
@@ -2,11 +2,11 @@
 
 docker-compose build --no-cache
 docker-compose up -d
-docker-compose exec covid_19_seir_model bash update.sh
-docker-compose exec covid_19_seir_model bash -c "echo \"{}\" > output.json"
-docker-compose exec covid_19_seir_model bash generate.sh
+docker-compose exec -T covid_19_seir_model bash update.sh
+docker-compose exec -T covid_19_seir_model bash -c "echo \"{}\" > output.json"
+docker-compose exec -T covid_19_seir_model bash generate.sh
 
-docker-compose run web_build ash -c "cd /var/www && ash update.sh"
+docker-compose run -T web_build ash -c "cd /var/www && ash update.sh"
 
 docker-compose down
 
